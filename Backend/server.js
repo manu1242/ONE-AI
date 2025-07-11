45e00a97cf4b5cb107b5b90fc6059a3a00667f12
manu1242 <manoharyalla77@gmail.com>
2025-07-11 20:33:58 +0530

    Adding vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+  res.send("Working Priyaaa")
+})
+
 
 app.use("/api/openai", openaiRoutes);
 
@@ -22,5 +26,5 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB error:", err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
