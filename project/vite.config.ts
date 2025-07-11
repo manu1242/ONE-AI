3292dbaa14df479d8efd490b2890d10ae86656b9
manu1242 <manoharyalla77@gmail.com>
2025-07-11 21:02:22 +0530

    Deploying Front end

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,13 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./", // ✅ Ensures correct relative paths in production
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'], // ✅ optional if you had issues
   },
-  build: {
-    outDir: "dist",  // ✅ Required so Vercel knows what to deploy
-    emptyOutDir: true
-  }
+ 
 });
